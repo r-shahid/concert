17ff93eefb548b32e7f4f85143204792c3d94720
r-shahid <rianashahid@gmail.com>
2026-08-18 23:46:01 -0400

    handle opacity via react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Tickets from "../components/Tickets";
@@ -5,11 +6,32 @@ import "../styles/home.css";
 
 
 function Home({allConcerts}) {
+  const [headerOpacity, setHeaderOpacity] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+
+      const startFade = window.innerHeight * 0.79;
+      const endFade = window.innerHeight * 0.81;
+
+      if (scrollTop < startFade){
+        setHeaderOpacity(0)
+      } else if (scrollTop >= startFade && scrollTop <= startFade){
+        const opacityProgress = (scrollTop - startFade)/(endFade - startFade)
+        setHeaderOpacity(opacityProgress)
+      } else {
+        setHeaderOpacity(1)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <div className="Home">
       <Hero />
-      <div className="header">
+      <div className="header" style={{ opacity: headerOpacity}}>
         <Header />
       </div>
       <div className="home-content">
